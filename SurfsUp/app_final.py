1e998eeb7061cb27e97ef1bc4c0a802fc81694ab
HussamGoda <h_goda@hotmail.com>
2023-08-09 21:00:51 +1000

    Final codes

--- SurfsUp/app_final.py
+++ SurfsUp/app_final.py
@@ -48,7 +48,10 @@ def Climate_App():
     )
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------
-#
+# Convert the query results to a dictionary by using date as the key and prcp as the value.
+# Return the JSON representation of your dictionary.
+
+
 @app.route("/api/v1.0/precipitation")
 def percipitation():
 
@@ -87,6 +90,8 @@ def percipitation():
     return jsonify(all_dates_prcp)
 
 #----------------------------------------------------------------------------------------
+# Return a JSON list of stations from the dataset.
+# Returns jsonified data of all of the stations in the database
 
 @app.route("/api/v1.0/stations")
 def stations():
@@ -125,7 +130,9 @@ def stations():
     return jsonify(all_stations_data)
 
 #-------------------------------------------------------------------------------------------------
-
+# Query the dates and temperature observations of the most-active station for the previous year of data.
+# Return a JSON list of temperature observations for the previous year
+    
 @app.route("/api/v1.0/tobs")
 def temps():
 
@@ -158,7 +165,7 @@ def temps():
 
     session.close()
 
-    # Create a dictionary from the row data
+    # Create a dictionary from the row data (basic info for most active station)
 
     most_active_station_basic_info = []
     for basic_data in basic_data_of_most_active_station:
@@ -172,6 +179,7 @@ def temps():
 
         most_active_station_basic_info.append(basic_data_station_active_dict)
 
+    # Create a dictionary from the row data (to return dates and tobs as required by the challenge)
     all_temp_data_for_one_year = []
     for station, date, tobs in temp_data_for_one_year:
         temp_date_one_year_dict = {}
@@ -181,16 +189,21 @@ def temps():
 
         all_temp_data_for_one_year.append(temp_date_one_year_dict)
 
+#Jsonify both dictionaries
     return jsonify ({"Basic Information for The Most Active Station": most_active_station_basic_info},
                      {"Information for One Year ": all_temp_data_for_one_year})
 
 #-----------------------------------------------------------------------------------------------------
 
 @app.route("/api/v1.0/<start>")
+
 def start_date(start):
     session = Session(bind=engine)
 
+    #accept start date from Url
+
     date_start_from_url = datetime.strptime(start, "%Y-%m-%d")
+    
     columns_to_use = [Measurement.date, Measurement.tobs]  
 
     calculated_parameters = session.query(*columns_to_use).filter(Measurement.date >= date_start_from_url).filter(Measurement.tobs != "NaN").all()
@@ -220,6 +233,8 @@ def start_date_end_date(start, end):
     
     session = Session(bind=engine)
 
+    #accept start date and end date from Url
+   
     date_start_from_url_1 = datetime.strptime(start, "%Y-%m-%d")
     date_end_from_url = datetime.strptime(end,"%Y-%m-%d")
     
